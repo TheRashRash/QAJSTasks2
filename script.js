51d3d730e4bc58f8bc9b89ce038fe6021f136810
TheRashRash <TheRashRash@github.com>
2022-02-02 10:24:03 +0000

    Completed functions task

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ myArray.push("Third added");
 for(let i in myArray){
     console.log(myArray[i]);
 };
+
+
+// functions 
+
+// Create a function that takes in 2 parameters num1 and num2 and subtracts the two numbers.
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(subtract(1, 32));
+
+
+function welcome(name, age, gender){
+    return `Hi my name is ${name}, I am ${age} years old and I am a ${gender}.`;
+}
+
+console.log(welcome("Rasheed", 29, "Male"));
+
+powerUp = (n1, n2) => {
+    return n1 ** n2;
+}
+
+console.log(powerUp(2, 3));
+
